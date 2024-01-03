4eb40451aff68406552e2bc26721fb3478461002
DHugo324 <116426935+DHugo324@users.noreply.github.com>
2024-01-04 00:14:34 +0800

    update to prepare statement

--- code_practice/insert/insert.php
+++ code_practice/insert/insert.php
@@ -13,7 +13,7 @@ function login_alert()
     return false;
 }
 
-$conn = include_once("../../db/condb.php");
+include_once("../../db/condb.php");
 
 $title = $_POST["title"];
 $description = $_POST["description"];
@@ -22,15 +22,15 @@ $course_name = $_POST["course_name"];
 $url = $_POST["url"];
 $userid = $_SESSION['userid'];
 
-$sql = "INSERT INTO code_practice (title, description, topic, course_name, url, userid) 
-            VALUES ('" . $title . "', '" . $description . "', '" . $topic . "', '" . $course_name . "', '" . $url . "', '" . $userid . "')";
-
-if ($db->exec($sql)) {
-    echo "新增成功! 3 秒後將自動跳轉頁面<br>";
-    echo "<a href='../code_practice.php'>未成功跳轉頁面請點擊此</a>";
-    header("refresh:3;url=../code_practice.php");
-    exit;
-} else {
-    echo "新增失敗： " . $db->errorInfo()[2];
+if ($stmt = $db->prepare("INSERT INTO code_practice (title, description, topic, course_name, url, userid) VALUES (?, ?, ?, ?, ?, ?)")) {
+    $success = $stmt->execute(array($title, $description, $topic, $course_name, $url, $userid));
+    if ($db->exec($sql)) {
+        echo "新增成功! 3 秒後將自動跳轉頁面<br>";
+        echo "<a href='../code_practice.php'>未成功跳轉頁面請點擊此</a>";
+        header("refresh:3;url=../code_practice.php");
+        exit;
+    } else {
+        echo "新增失敗： " . $db->errorInfo()[2];
+    }
 }
 ?>
