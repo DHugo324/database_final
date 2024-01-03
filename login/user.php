59eec9b0e95cf951bdb378e739a4fed683f032a0
DHugo324 <116426935+DHugo324@users.noreply.github.com>
2024-01-03 21:45:14 +0800

    update user.php

--- login/user.php
+++ login/user.php
@@ -15,16 +15,45 @@ $username = $_SESSION["username"];
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>歡迎</title>
+    <title>用戶頁面</title>
+    <style>
+        .add {
+            padding: 5px 10px;
+            background-color: #5cb85c;
+            color: white;
+            font-weight: bold;
+            font-size: large;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        .add:hover {
+            background-color: #77c477;
+        }
+    </style>
 </head>
 
 <body>
+    <!-- <header>
+        <a href="../index.html">首頁</a>
+        <a href="logout.php">登出</a>
+    </header> -->
     <h1>你好，
         <?php echo $username; ?>
     </h1>
-    <a href="logout.php">登出</a>
-    <a href="../code_practice/insert/index.php">前往新增程式練習頁面</a>
-    <a href="../note/insert/index.php">前往新增筆記頁面</a>
+    <div>
+        <h2>我的筆記 <button class="add" onclick="window.location.href = '../note/insert/index.php'">新增</button>
+        </h2>
+        <div></div>
+    </div>
+    <hr>
+    <div>
+        <h2>我的程式練習 <button class="add" onclick="window.location.href = '../code_practice/insert/index.php'">新增</button>
+        </h2>
+        <div></div>
+    </div>
+    <hr>
 </body>
 
 </html>
