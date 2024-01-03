3e2b7acb816579633fafcc803f90f009d70ef2c2
DHugo324 <116426935+DHugo324@users.noreply.github.com>
2024-01-03 21:43:57 +0800

    update css

--- code_practice/insert/index.php
+++ code_practice/insert/index.php
@@ -23,22 +23,27 @@ function login_alert()
 </head>
 
 <body>
-    <h1>新增程式練習</h1>
-    <form method="post" action="insert.php">
-        標題：
-        <input type="text" name="title" required><br><br>
-        描述：
-        <input type="text" name="description"><br><br>
-        主題：
-        <input type="text" name="topic"><br><br>
-        課程名稱：
-        <input type="text" name="course_name"><br><br>
-        網址：
-        <input type="url" name="url" required><br><br>
-        <input type="submit" value="新增" name="submit">
-        <input type="reset" value="重設" name="reset">
-    </form>
-    <a href="../code_practice.php">返回</a>
+    <div>
+        <img src="../../image/back.png" title="返回" width="50px" height="50px" style="cursor: pointer;"
+            onclick="history.back()">
+    </div>
+    <main style="width:100%; display: flex; flex-direction: column; align-items: center;">
+        <h1>新增程式練習</h1>
+        <form method="post" action="insert.php">
+            標題：
+            <input type="text" name="title" required><br><br>
+            描述：
+            <input type="text" name="description"><br><br>
+            主題：
+            <input type="text" name="topic"><br><br>
+            課程名稱：
+            <input type="text" name="course_name"><br><br>
+            網址：
+            <input type="url" name="url" required><br><br>
+            <input type="submit" value="新增" name="submit">
+            <input type="reset" value="重設" name="reset">
+        </form>
+    </main>
 </body>
 
 </html>
