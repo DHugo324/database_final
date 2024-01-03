8b8ab79b29061d7bc1045c9fc37c569600a5c0a2
DHugo324 <116426935+DHugo324@users.noreply.github.com>
2024-01-03 22:05:03 +0800

    rename and move file

--- code_practice/edit/index.php
+++ code_practice/edit/index.php
@@ -9,9 +9,9 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 function login_alert()
 {
     echo "請先登入! 3 秒後將自動跳轉登入頁面<br>";
-    echo "<a href='../../login/index.php'>未成功跳轉頁面請點擊此</a>";
-    echo "或是<a href='../../index.html'>返回首頁</a>";
-    header("refresh:3;url=../../login/index.php");
+    echo "<a href='../../account/index.php'>未成功跳轉頁面請點擊此</a>";
+    echo "或是<a href='../../home.php'>返回首頁</a>";
+    header("refresh:3;url=../../account/index.php");
     return false;
 }
 ?>
