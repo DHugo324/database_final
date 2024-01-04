283229c53876598982d732c96100377061ebdf4c
DHugo324 <116426935+DHugo324@users.noreply.github.com>
2024-01-04 15:27:32 +0800

    update contact page

--- contact/index.php
+++ contact/index.php
@@ -1,3 +1,22 @@
 <?php
-echo "聯絡我們 & 回報Bug";
 ?>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>編輯介面</title>
+</head>
+
+<body>
+    <main style="width:100%; height: 100%; display: flex; flex-direction: column; align-items: center;">
+        <h1>有問題或意見歡迎填寫表單！</h1>
+        <div style="height:100%">
+            <iframe
+                src="https://docs.google.com/forms/d/e/1FAIpQLSe5bl3C4X4781XQxAdG0pT0DjnBYkn8iGwvCV0LSCCT_cUDSg/viewform?embedded=true"
+                width="640" height="100%" frameborder="0" marginheight="0" marginwidth="0">載入中…
+            </iframe>
+        </div>
+    </main>
+</body>
+
+</html>
